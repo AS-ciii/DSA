104ea7020176e5cb91060fa9322190b7440e3565
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2025-06-21 23:00:06 +0530

    Day 156

---
+++ Two_Pointer_Technique/PoliceAndThieves.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int catchThieves(char[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int police = 0, thief = 0, count = 0;
+        
+        while(police < n && thief < n){
+            while(police < n && arr[police] != 'P') police++;
+            while(thief < n && arr[thief] != 'T') thief++;
+            
+            if(police < n && thief < n){
+                if(Math.abs(police - thief) <= k){
+                    count++;
+                    police++;
+                    thief++;
+                }else if(thief < police){
+                    thief++;
+                }else{
+                    police++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
