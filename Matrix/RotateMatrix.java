fc12a04684471b2412f0875bb5ab27f6b10028db
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2025-08-11 23:04:20 +0530

    Day 196

---
+++ Matrix/RotateMatrix.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for(int i=0; i<matrix.length; i++) {
+            reverse(matrix, i, 0, matrix.length-1);
+        }
+    }
+
+    public void reverse(int[][] matrix, int i, int left, int right) {
+        while(left<right) {
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
