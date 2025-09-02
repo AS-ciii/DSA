bfb2f80812c76f60dfa2b5fd0b08b965e64f6d1d
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2025-09-02 23:23:04 +0530

    Day 215

---
+++ Linked_List/SwapKthNodesFromEnds.java
@@ -0,0 +1,29 @@
+class Solution {
+    public Node swapKth(Node head, int k) {
+        // code here
+        // ArrayList<Integer> ob=new ArrayList<Integer>();
+        Node ptr=head;
+        int c=0;
+        while(ptr!=null){
+            c++;
+            // ob.add(ptr.data)
+            ptr=ptr.next;
+        }
+        if(k>c)
+        return head;
+        int i=0;
+        ptr=head;
+        for( i=0;i<k-1;i++){
+            ptr=ptr.next;
+        }
+        Node link=ptr;
+        int temp=ptr.data;
+        ptr=head;
+        for(i=0;i<c-k;i++){
+            ptr=ptr.next;
+        }
+        link.data=ptr.data;
+        ptr.data=temp;
+        return head;
+    }
+}
