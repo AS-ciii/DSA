1cc12c92ffecb88f5cbfa56d61e7b9eb809ef2d8
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2025-10-15 22:41:28 +0530

    Day 255

---
+++ Tree/KthSmallestInBST.java
@@ -0,0 +1,33 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    
+    PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+    public int kthSmallest(Node root, int k) {
+        // code here
+        trav(root);
+        if(k > pq.size()) return -1;
+        while(k-- > 1) pq.poll();
+        return pq.peek();
+    }
+    
+    void trav(Node root)
+    {
+        if(root == null) return;
+        trav(root.left);
+        pq.add(root.data);
+        trav(root.right);
+    }
+}
