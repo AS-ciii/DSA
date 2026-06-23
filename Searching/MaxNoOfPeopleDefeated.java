560b7c00c42a74e2f0010bfde28e6e0aa249a65d
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2026-06-23 23:23:31 +0530

    Day 464

---
+++ Searching/MaxNoOfPeopleDefeated.java
@@ -0,0 +1,21 @@
+class Solution {
+    int maxPeopleDefeated(int p) {
+        long l = 1;
+        long r = (long) Math.sqrt(p);
+        int ans = 0;
+
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long sum = (mid * (mid + 1) * (2 * mid + 1)) / 6;
+
+            if (sum <= p) {
+                ans = (int) mid; // valid number of people
+                l = mid + 1;     // try for more
+            } else {
+                r = mid - 1;     // too large
+            }
+        }
+
+        return ans;
+    }
+}
