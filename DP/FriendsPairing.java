948dd4668df211bc5da538a204b3cc26280dec39
Ajay Shenoy P <154616179+AS-ciii@users.noreply.github.com>
2026-08-07 23:04:04 +0530

    Day 494

---
+++ DP/FriendsPairing.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    countFriendsPairings(n) {
+        // code here
+        
+        if(n<=2){
+            return n
+        }
+        let dp=new Array(n+1);
+        dp[1]=1;
+        dp[2]=2
+        for(let i=3;i<=n;i++){
+            dp[i]=dp[i-1]+(i-1)*dp[i-2];
+        }
+        return dp[n]
+    }
+}
